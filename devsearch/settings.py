c3287f7e9eebf5836a315a2494ceff1819aa9b8c
Alex <alexandruchelariu302@gmail.com>
2025-10-04 15:49:58 +0300

    Add email password reset and welcome email features
    Configured SMTP email backend and moved sensitive credentials to things.py. Added password reset URLs and updated login template to use Django's password reset flow. Implemented welcome email on profile creation and improved user deletion error handling in signals.py. Updated account template to hide 'Send Message' button for own profile.

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from things import SECRET_KEY, EMAIL_HOST_PASSWORD, EMAIL_HOST_USER
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*mmt(ueibqp)88b7yg-$ra21avz&e#@j*tj@y1v!qwxi34e&*d'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -139,3 +140,15 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 MEDIA_URL = '/images/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = '587'
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = EMAIL_HOST_USER
+
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD 
--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,10 @@
 
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 
 from django.contrib.auth.models import User
 from .models import Profile
@@ -17,6 +21,16 @@ def createProfile(sender, instance, created, **kwargs):
             email = user.email,
             name = user.first_name,
         )
+        subject = "Welcome to DevSearch!"
+        message = 'We are glad you are here'
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
+        )
+
 
 
 def updateUser(sender, instance, created, **kwargs):
@@ -28,9 +42,15 @@ def updateUser(sender, instance, created, **kwargs):
         user.email = profile.email
         user.save()
 
+
+
 def deleteUser(sender, instance, **kwargs):
-    user = instance.user
-    user.delete()
+    try:
+        user = instance.user
+        if user:
+            user.delete()
+    except (User.DoesNotExist, ObjectDoesNotExist):
+        pass
 
 post_save.connect(createProfile, sender=User)
 post_save.connect(updateUser, sender=Profile)
